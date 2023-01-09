360c1107dc1c819c7af19a58376bbac857460174
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-01-09 19:58:25 +0530

    connect to DB

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,66 @@
 package main
 
-const webPort := "80"
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
 
-func main() {}
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+func main() {
+	db := initDB()
+	db.Ping()
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("can't connect to database")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgres not yet ready...")
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+
+		if counts > 10 {
+			return nil
+		}
+
+		log.Println("Backing off for one second")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
