3616ca99776e1b9665efd7ed9ea644283abbea71
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-01-16 21:27:53 +0530

    Add stub routes and handler

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,8 +11,16 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.SessionLoad)
 
 	mux.Get("/", app.HomePage)
+
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate-account", app.ActivateAccount)
 
 	return mux
 }
